f2a4dd5f37f759d0397fde2a32d0882b299dddb6
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-04-11 11:08:14 +0200

    added nav

--- src/app/henry/page.jsx
+++ src/app/henry/page.jsx
@@ -1,12 +1,20 @@
 async function getHenry() {
     const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
-    const data = await res.json()
+    const data = await res.json();
     return data;
+}
+
+export async function generateMetadata() {
+    const { name, age } = await getHenry();
+
+    return {
+        title: name,
+        description: `${name} is ${age} years old `,
+    }
 }
 
 export default async function HenryPage() {
     const data = await getHenry();
-    console.log(data);
     return (
         <main>
             <h1>{data.name}</h1>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,15 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "Frontpage",
+  description: "This is the frontpage of the website",
+};
 
 export default function Home() {
   return <main>
     <h1>Hello World :D </h1>
+    <Link href={"/henry"} prefetch={false}>
+      Henry
+    </Link>
   </main>;
 }
